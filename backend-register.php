d0f5a4c1d2d4f9c8cfa7a4ce4658da6723fed06d
TheParadox20 <34806448+TheParadox20@users.noreply.github.com>
2020-09-09 10:09:14 +0300

    backend

--- backend-register.php
+++ backend-register.php
@@ -5,7 +5,7 @@ $un=$_POST['name'];
 $Email=$_POST['email'];
 $pass=password_hash($_POST['password'] , PASSWORD_DEFAULT);
 $link = new mysqli($Hn , $User , $Pass , $Db );
-$submit = "INSERT INTO `User` (`FirstName` , `LastName` , `UserName` , `Email` , `Location` , `Password`) VALUES ('$fn' , '$ln' , '$un' , '$Email' , '$Location' , '$pass')";
+$submit = "INSERT INTO `User` (`Name` , `Email` ,`Password`) VALUES ($un' , '$Email' , $pass')";
 $link->query($submit);
 $link->close();
 include "login.php";
