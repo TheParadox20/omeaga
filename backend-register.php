6c8980eb280e37f37502fc06028a3fc92a1d4002
TheParadox20 <34806448+TheParadox20@users.noreply.github.com>
2020-09-11 15:07:48 +0300

    backend bug

--- backend-register.php
+++ backend-register.php
@@ -5,7 +5,7 @@ $un=$_POST['name'];
 $Email=$_POST['email'];
 $pass=password_hash($_POST['password'] , PASSWORD_DEFAULT);
 $link = new mysqli($Hn , $User , $Pass , $Db );
-$submit = "INSERT INTO `user` (`Name` , `Email` ,`Password`) VALUES ($un' , '$Email' , $pass')";
+$submit = "INSERT INTO `user` (`Name` , `Email` ,`Password`) VALUES ('$un' , '$Email' , '$pass')";
 $link->query($submit);
 $link->close();
 include "login.php";
